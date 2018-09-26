8b3ca90081f800140f7eedbb20c565bbb5c29599
lariso <leandro.ariso@gmail.com>
2018-09-26 16:34:43 -0300

    26-09-18

--- app/src/main/java/multivac/app0/MainActivity.java
+++ app/src/main/java/multivac/app0/MainActivity.java
@@ -3,31 +3,38 @@ package multivac.app0;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Toast;
-import android.app.Activity;
+import android.view.View;
+import android.widget.Button;
 
 import com.github.johnpersano.supertoasts.library.Style;
 import com.github.johnpersano.supertoasts.library.SuperActivityToast;
-import com.github.johnpersano.supertoasts.library.utils.PaletteUtils;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button boton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        SuperActivityToast.create(this, new Style(), Style.TYPE_BUTTON)
-                .setButtonText("UNDO")
-                .setButtonIconResource(R.drawable.selector_button_standard)
-                .setOnButtonClickListener("good_tag_name", null, null)
-                .setProgressBarColor(Color.WHITE)
-                .setText("Email deleted")
-                .setDuration(Style.DURATION_LONG)
-                .setFrame(Style.FRAME_LOLLIPOP)
-                .setColor(PaletteUtils.getSolidColor(PaletteUtils.MATERIAL_PURPLE))
-                .setAnimations(Style.ANIMATIONS_POP).show();
+        boton = findViewById(R.id.button_main_1);
 
+        boton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SuperActivityToast.create(MainActivity.this, new Style(), Style.TYPE_BUTTON)
+                        //.setButtonText("UNDO")
+                        //.setButtonIconResource(R.drawable.selector_button_standard)
+                        //.setOnButtonClickListener("good_tag_name", null, null)
+                        .setProgressBarColor(Color.WHITE)
+                        .setText("this works!")
+                        .setDuration(Style.DURATION_LONG)
+                        .setFrame(Style.FRAME_LOLLIPOP)
+                        .setColor(getResources().getColor(R.color.accent, getTheme()))
+                        .setAnimations(Style.ANIMATIONS_POP).show();
+            }
+        });
     }
 }
